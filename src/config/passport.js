adaf1a949edcccdea9273b80148b78d70cb30b40
Averous13 <92216719+Averous13@users.noreply.github.com>
2026-07-31 22:25:33 +0700

    fix: auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `http://localhost:${process.env.PORT || 5000}/api/auth/google/callback`
+            callbackURL: `${CLIENT_URL}/api/auth/google/callback`
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
